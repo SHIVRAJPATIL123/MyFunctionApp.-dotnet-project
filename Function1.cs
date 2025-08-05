6fe4da1f248be676a019179dd5ab33922a77a2b5
SHIVRAJPATIL123 <118585118+SHIVRAJPATIL123@users.noreply.github.com>
2025-08-05 16:24:26 +0530

    Update Function1.cs

--- Function1.cs
+++ Function1.cs
@@ -1,20 +1,18 @@
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
-using Microsoft.Extensions.Logging;
 using System.Net;
 
-public class Function1
+namespace MyFunctionApp
 {
-    [Function("Function1")]
-    public HttpResponseData Run(
-        [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req,
-        FunctionContext context)
+    public class Function1
     {
-        var logger = context.GetLogger("Function1");
-        logger.LogInformation("C# HTTP trigger function processed a request.");
-
-        var response = req.CreateResponse(HttpStatusCode.OK);
-        response.WriteString("Hello from Azure Function App (.NET 8)!");
-        return response;
+        [Function("Function1")]
+        public HttpResponseData Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
+        {
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.WriteString("Hello from .NET 8 Isolated Azure Function!");
+            return response;
+        }
     }
 }
