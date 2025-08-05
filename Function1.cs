6c8b134c926ab32ebbe55197603f4d27b0e765bb
SHIVRAJPATIL123 <118585118+SHIVRAJPATIL123@users.noreply.github.com>
2025-08-05 16:29:41 +0530

    Update Function1.cs

--- Function1.cs
+++ Function1.cs
@@ -1,18 +1,25 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
-using Microsoft.Azure.Functions.Worker.Http;
-using System.Net;
+using Microsoft.Extensions.Logging;
 
 namespace MyFunctionApp
 {
     public class Function1
     {
-        [Function("Function1")]
-        public HttpResponseData Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
+        private readonly ILogger<Function1> _logger;
+
+        public Function1(ILogger<Function1> logger)
         {
-            var response = req.CreateResponse(HttpStatusCode.OK);
-            response.WriteString("Hello from .NET 8 Isolated Azure Function!");
-            return response;
+            _logger = logger;
+        }
+
+        // The HttpTrigger attribute and AuthorizationLevel are defined in the Microsoft.Azure.Functions.Worker namespace.
+        [Function("Function1")]
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            return new OkObjectResult("Welcome to Azure Functions!");
         }
     }
 }
